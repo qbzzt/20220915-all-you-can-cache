c6ec6389a1ae8544fbb2739d3a9392b2cc5764c1
Ori Pomerantz <qbzzt1@gmail.com>
2022-08-29 17:03:04 -0500

    Add the write once, read many contract

---
+++ src/WORM.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+import "./Cache.sol";
+
+
+
+contract WORM is Cache {
+
+    struct Entry {  
+        uint value;
+        address writtenBy;
+        uint writtenAtBlock;
+    }   // struct Entry
+
+    mapping(uint => Entry) public entries;
+
+    // An entry has been written. There is no need for the event to specify by whom or at what block,
+    // because the event is connected to the transaction that initiated it.
+    event EntryWritten(uint key, uint value);
+
+    function writeEntry(uint _key, uint _value) public {
+        require(entries[_key].writtenBy == address(0), "entry already written");
+
+        entries[_key].writtenBy = tx.origin;
+        entries[_key].value = _value;
+        entries[_key].writtenAtBlock = block.number;
+    }   // writeEntry
+
+
+    function writeEntryCached() external {
+        uint[] memory params = _readParams(2);
+        writeEntry(params[0], params[1]);
+    }    // writeEntryCached
+
+    // Make it easier to call us
+    // Function signature for writeEntryCached(), courtesy of
+    // https://www.4byte.directory/signatures/?bytes4_signature=0xe4e4f2d3
+    bytes4 constant public WRITE_ENTRY_CACHED = 0xe4e4f2d3;
+
+
+    function readEntry(uint key) public view 
+        returns (uint _value, address _writtenBy, uint _writtenAtBlock)
+    { 
+        Entry memory entry = entries[key];
+        require(entry.writtenBy != address(0), "no entry for this key");
+        return (entry.value, entry.writtenBy, entry.writtenAtBlock);
+    }
+
+
+}  // WORM
---
+++ test/WORM.t.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+
+// Need to run `forge test -vv` for the console.
+import "forge-std/console.sol";
+import "src/WORM.sol";
+
+contract WORMTest is Test {
+    WORM worm;
+
+    function setUp() public {
+        worm = new WORM();        
+    }
+
+    function testWReadWrite() public {
+        worm.writeEntry(0xDEAD, 0x60A7);
+
+        vm.expectRevert(bytes("entry already written"));
+        worm.writeEntry(0xDEAD, 0xBEEF);
+
+        uint val;
+        uint blockNum;
+        address writer;
+        (val, writer, blockNum) = worm.readEntry(uint(0xDEAD));
+        assertEq(val, 0x60A7);
+
+        vm.expectRevert(bytes("no entry for this key"));
+        (val, writer, blockNum) = worm.readEntry(uint(0xBEEF));       
+    }    // testReadWrite    
+
+    function testReadWriteCached() public {
+        uint cacheGoat = worm.cacheWrite(0x60A7);
+        uint cacheBeef = worm.cacheWrite(0xBEEF);
+        uint cacheDead = worm.cacheWrite(0xDEAD);
+        uint cacheSage = worm.cacheWrite(0x5A6E);
+
+        assertEq(cacheGoat, 1);
+        assertEq(cacheBeef, 2);
+        assertEq(cacheDead, 3);
+        assertEq(cacheSage, 4); 
+
+        assertEq(uint32(worm.WRITE_ENTRY_CACHED()), 0xe4e4f2d3);
+
+        bool _success;
+        bytes memory _callInput;
+
+        // Write entries
+        _callInput = bytes.concat(
+            worm.WRITE_ENTRY_CACHED(),
+            bytes1(uint8(cacheGoat)),    // Key
+            bytes1(uint8(cacheSage))     // Value
+        );
+        (_success,) = address(worm).call(_callInput);
+        assertEq(_success, true);
+
+        _callInput = bytes.concat(
+            worm.WRITE_ENTRY_CACHED(),
+            bytes1(uint8(cacheDead)), bytes1(uint8(cacheBeef)) 
+        );
+        (_success,) = address(worm).call(_callInput);
+        assertEq(_success, true);        
+
+        // Fail to overwrite
+        _callInput = bytes.concat(
+            worm.WRITE_ENTRY_CACHED(),
+            bytes1(uint8(cacheDead)), bytes1(uint8(cacheGoat)) 
+        );
+        (_success,) = address(worm).call(_callInput);
+        assertEq(_success, false);  
+
+        uint val;
+
+        (val,,) = worm.readEntry(0xDEAD);
+        assertEq(val, 0xBEEF);
+
+
+        (val,,) = worm.readEntry(0x60A7);
+        assertEq(val, 0x5A6E);
+
+        vm.expectRevert(bytes("no entry for this key"));
+        (val,,) = worm.readEntry(0xBEEF);
+
+        vm.expectRevert(bytes("no entry for this key"));        
+        (val,,) = worm.readEntry(0x5A6E);    
+    }   // testReadWriteCached
+
+
+    // This tests every part of the system together
+    // The parameters make Foundry turn on the fuzzer
+    function testWithEncodeVal(uint a, uint b, uint c) public {
+        bool _success;
+        bytes memory _callInput;
+        uint retVal;
+
+        _callInput = bytes.concat(
+            worm.WRITE_ENTRY_CACHED(), worm.encodeVal(a), worm.encodeVal(b));
+        (_success,) = address(worm).call(_callInput);
+        assertEq(_success, true);
+        assertEq(_callInput.length, 4+33*2);
+        (retVal,,) = worm.readEntry(a);
+        assertEq(retVal, b); 
+
+        // Only create the reverse if a!=b, otherwise we'll get a failure
+        if (a != b) {
+            _callInput = bytes.concat(
+                worm.WRITE_ENTRY_CACHED(), worm.encodeVal(b), worm.encodeVal(a));
+            (_success,) = address(worm).call(_callInput);
+            assertEq(_success, true);
+            assertEq(_callInput.length, 4+1*2);
+            (retVal,,) = worm.readEntry(b);
+            assertEq(retVal, a); 
+        }
+
+
+        // If c is not already in the cache
+        if (a != c && b != c) {
+            _callInput = bytes.concat(
+                worm.WRITE_ENTRY_CACHED(), worm.encodeVal(c), worm.encodeVal(a));
+            (_success,) = address(worm).call(_callInput);
+            assertEq(_success, true);
+            assertEq(_callInput.length, 4+33+1);
+            (retVal,,) = worm.readEntry(c);
+            assertEq(retVal, a); 
+        }
+
+
+    }  // testWithEncodeVal
+
+}  // WORMTest
